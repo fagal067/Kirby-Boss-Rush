e6184d770be80cb9bf309c27ae54eb2a99886515
fagal067 <75336760+fagal067@users.noreply.github.com>
2020-12-03 13:06:07 -0700

    Add files via upload

---
+++ game - Copy/game/Form2.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Bullet = New System.Windows.Forms.PictureBox()
+        Me.yay = New System.Windows.Forms.PictureBox()
+        Me.gameover = New System.Windows.Forms.PictureBox()
+        Me.replayButton = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Bullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.yay, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gameover, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Turquoise
+        Me.PictureBox2.Image = Global.game.My.Resources.Resources.starship__1_
+        Me.PictureBox2.Location = New System.Drawing.Point(101, 373)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(166, 159)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 17
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.game.My.Resources.Resources.starship__1_
+        Me.PictureBox1.Location = New System.Drawing.Point(60, 140)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(166, 159)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Bullet
+        '
+        Me.Bullet.BackColor = System.Drawing.Color.Transparent
+        Me.Bullet.Image = Global.game.My.Resources.Resources.star
+        Me.Bullet.Location = New System.Drawing.Point(12, 449)
+        Me.Bullet.Name = "Bullet"
+        Me.Bullet.Size = New System.Drawing.Size(83, 83)
+        Me.Bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Bullet.TabIndex = 18
+        Me.Bullet.TabStop = False
+        Me.Bullet.Visible = False
+        '
+        'yay
+        '
+        Me.yay.BackColor = System.Drawing.Color.Transparent
+        Me.yay.Image = Global.game.My.Resources.Resources.win2
+        Me.yay.Location = New System.Drawing.Point(357, 241)
+        Me.yay.Name = "yay"
+        Me.yay.Size = New System.Drawing.Size(683, 321)
+        Me.yay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.yay.TabIndex = 19
+        Me.yay.TabStop = False
+        Me.yay.Visible = False
+        '
+        'gameover
+        '
+        Me.gameover.BackColor = System.Drawing.Color.Transparent
+        Me.gameover.Image = Global.game.My.Resources.Resources.gameover
+        Me.gameover.Location = New System.Drawing.Point(381, 241)
+        Me.gameover.Name = "gameover"
+        Me.gameover.Size = New System.Drawing.Size(634, 321)
+        Me.gameover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.gameover.TabIndex = 20
+        Me.gameover.TabStop = False
+        Me.gameover.Visible = False
+        '
+        'replayButton
+        '
+        Me.replayButton.Font = New System.Drawing.Font("Segoe Print", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.replayButton.Location = New System.Drawing.Point(611, 568)
+        Me.replayButton.Name = "replayButton"
+        Me.replayButton.Size = New System.Drawing.Size(166, 159)
+        Me.replayButton.TabIndex = 21
+        Me.replayButton.Text = "Retry"
+        Me.replayButton.UseVisualStyleBackColor = True
+        Me.replayButton.Visible = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1396, 803)
+        Me.Controls.Add(Me.replayButton)
+        Me.Controls.Add(Me.gameover)
+        Me.Controls.Add(Me.yay)
+        Me.Controls.Add(Me.Bullet)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Bullet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.yay, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gameover, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Bullet As PictureBox
+    Friend WithEvents yay As PictureBox
+    Friend WithEvents gameover As PictureBox
+    Friend WithEvents replayButton As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ game - Copy/game/Form2.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form2
+    Sub move(p As PictureBox, x As Integer, y As Integer)
+        p.Location = New Point(p.Location.X + x, p.Location.Y + y)
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown, replayButton.KeyDown
+        If gameover.Visible Or yay.Visible Then
+            Exit Sub
+        End If
+        Select Case e.KeyCode
+
+            Case Keys.R
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+                Me.Refresh()
+            Case Keys.Up, Keys.W
+                MoveTo(PictureBox1, 0, -15)
+
+            Case Keys.Down, Keys.S
+                MoveTo(PictureBox1, 0, 15)
+
+            Case Keys.Left, Keys.A
+                MoveTo(PictureBox1, -15, 0)
+
+            Case Keys.Right, Keys.D
+                MoveTo(PictureBox1, 15, 0)
+
+            Case Keys.Space
+                Bullet.Location = PictureBox1.Location
+                Bullet.Visible = True
+                Timer2.Enabled = True
+        End Select
+    End Sub
+    Sub follow(p As PictureBox)
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(PictureBox1.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            p.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+
+    Public Sub chase(p As PictureBox)
+        Dim x, y As Integer
+        If p.Location.X > PictureBox1.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(p, x, 0)
+        If p.Location.Y < PictureBox1.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(p, x, y)
+    End Sub
+
+
+
+    Function Collision(p As PictureBox, t As String, Optional ByRef other As Object = vbNull)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If obj.Visible AndAlso p.Bounds.IntersectsWith(obj.Bounds) And obj.Name.ToUpper.Contains(t.ToUpper) Then
+                col = True
+                other = obj
+            End If
+        Next
+        Return col
+    End Function
+
+    'Return true or false if moving to the new location is clear of objects ending with t
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+    End Function
+    'Moves and object (won't move onto objects containing  "wall" and shows green if object ends with "win"
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+
+        If p.Name = "PictureBox1" And Collision(p, "WIN") Then
+
+            Return
+
+        End If
+    End Sub
+
+
+
+
+End Class
